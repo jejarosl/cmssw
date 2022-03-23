9f2268c4baf3003d7f0d7f3808940afa92d22b47
Felice Pantaleo <felice.pantaleo@cern.ch>
2022-03-23 12:41:35 +0100

    enable ticlv4 modifier

--- RecoHGCal/TICL/python/iterativeTICL_cff.py
+++ RecoHGCal/TICL/python/iterativeTICL_cff.py
@@ -3,9 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from RecoHGCal.TICL.FastJetStep_cff import *
 from RecoHGCal.TICL.CLUE3DHighStep_cff import *
 from RecoHGCal.TICL.CLUE3DLowStep_cff import *
-from RecoHGCal.TICL.MIPStep_cff import *
-from RecoHGCal.TICL.TrkEMStep_cff import *
-from RecoHGCal.TICL.TrkStep_cff import *
 from RecoHGCal.TICL.EMStep_cff import *
 from RecoHGCal.TICL.HADStep_cff import *
 
@@ -66,9 +63,6 @@ ticlLayerTileHFNose = ticlLayerTileProducer.clone(
 ticlLayerTileHFNoseTask = cms.Task(ticlLayerTileHFNose)
 
 iterHFNoseTICLTask = cms.Task(ticlLayerTileHFNoseTask
-    ,ticlHFNoseTrkEMStepTask
     ,ticlHFNoseEMStepTask
-    ,ticlHFNoseTrkStepTask
     ,ticlHFNoseHADStepTask
-    ,ticlHFNoseMIPStepTask
 )
